3c9ea891a07065966edfdc83cca43e7774cf88fc
venura <vliyanagama@gmail.com>
2019-10-01 08:45:56 +0530

    elecslal

---
+++ elecslal.net/calendar/setd21b.php
@@ -0,0 +1,14 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from elecslal.net/calendar/set.php?return=aHR0cDovL2VsZWNzbGFsLm5ldC9jYWxlbmRhci92aWV3LnBocD92aWV3PWRheSZ0aW1lPTE1Nzg4MDUyMDAmY291cnNlPTE%3D&sesskey=iJhtJyp6Mi&var=showcourses by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Sep 2019 19:22:43 GMT -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=view65e5.html?view=day%26time=1578805200%26course=1"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="view65e5.html?view=day%26time=1578805200%26course=1"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from elecslal.net/calendar/set.php?return=aHR0cDovL2VsZWNzbGFsLm5ldC9jYWxlbmRhci92aWV3LnBocD92aWV3PWRheSZ0aW1lPTE1Nzg4MDUyMDAmY291cnNlPTE%3D&sesskey=iJhtJyp6Mi&var=showcourses by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Sep 2019 19:22:48 GMT -->
+</HTML>
